8832a98bdc7ad8148f5d624e74f535ea031ea493
ranger163 <ahmed.nassar163@gmail.com>
2020-05-25 22:20:55 +0200

    Wrapping up all in the MainActivity "popular movies screen". Closes #22

--- app/src/main/java/me/inassar/tmbd/view/popularmovies/MainActivity.kt
+++ app/src/main/java/me/inassar/tmbd/view/popularmovies/MainActivity.kt
@@ -1,13 +1,76 @@
 package me.inassar.tmbd.view.popularmovies
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import me.inassar.tmbd.R
+import me.inassar.tmbd.data.api.ApiCall
+import me.inassar.tmbd.data.api.AppApi
+import me.inassar.tmbd.data.repository.NetworkState
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainActivityViewModel
+
+    lateinit var movieRepository: MoviePagedListRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService: AppApi = ApiCall.getClient()
+
+        movieRepository = MoviePagedListRepository(apiService)
+
+        viewModel = getViewModel()
+
+        val movieAdapter = PopularMoviePagedListAdapter(this)
+
+        val gridLayoutManager = GridLayoutManager(this, 3)
+
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+                return if (viewType == movieAdapter.movieViewType) 1    // Movie_VIEW_TYPE will occupy 1 out of 3 span
+                else 3                                              // NETWORK_VIEW_TYPE will occupy all 3 span
+            }
+        }
+
+
+        rv_movie_list.layoutManager = gridLayoutManager
+        rv_movie_list.setHasFixedSize(true)
+        rv_movie_list.adapter = movieAdapter
+
+        viewModel.moviePagedList.observe(this, Observer {
+            movieAdapter.submitList(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar_popular.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error_popular.visibility =
+                if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!viewModel.listIsEmpty()) {
+                movieAdapter.setNetworkState(it)
+            }
+        })
+
     }
+
+    private fun getViewModel(): MainActivityViewModel {
+        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MainActivityViewModel(movieRepository) as T
+            }
+        })[MainActivityViewModel::class.java]
+    }
+
 }
