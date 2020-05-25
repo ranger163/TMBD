27818db643164b7bf11945f9dc5215c4da1190ba
ranger163 <ahmed.nassar163@gmail.com>
2020-05-25 17:49:46 +0200

    Creating MovieDetailsActivity screen . Closes #2

---
+++ app/src/main/java/me/inassar/tmbd/view/MovieDetailsActivity.kt
@@ -0,0 +1,13 @@
+package me.inassar.tmbd.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.inassar.tmbd.R
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
