39a46d858cd903ec0cfb64763ea2de97faa56448
ranger163 <ahmed.nassar163@gmail.com>
2020-05-25 19:26:25 +0200

    Creating API client for TMDB. Closes #6

---
+++ app/src/main/java/me/inassar/tmbd/data/api/AppApi.kt
@@ -0,0 +1,19 @@
+package me.inassar.tmbd.data.api
+
+import io.reactivex.Single
+import me.inassar.tmbd.AppConfig
+import me.inassar.tmbd.Constants
+import me.inassar.tmbd.data.model.MovieDetailsModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AppApi {
+    @GET(Constants.MOVIE_DETAILS_URL)
+    fun getMovieDetails(@Path(Constants.PATH_PARAM_MOVIE_ID) movieId: Int): Single<MovieDetailsModel>
+}
+
+object ApiCall {
+    fun getClient(): AppApi {
+        return AppConfig().retrofit.create(AppApi::class.java)
+    }
+}
