af868ce98ad354ed25039a4e5f3f21230ba6150f
ranger163 <ahmed.nassar163@gmail.com>
2020-05-25 20:51:01 +0200

    Adding Constants.kt class

---
+++ app/src/main/java/me/inassar/tmbd/Constants.kt
@@ -0,0 +1,10 @@
+package me.inassar.tmbd
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
+    const val THE_MOVIE_DB_API_KEY = "ADD YOUR API KEY HERE"
+    const val MOVIE_DETAILS_URL="movie/{movie_id}"
+    const val PATH_PARAM_MOVIE_ID="movie_id"
+    const val PATH_PARAM_API_KEY = "api_key"
+}
