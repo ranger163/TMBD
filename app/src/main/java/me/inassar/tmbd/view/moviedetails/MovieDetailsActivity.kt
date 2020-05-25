8e14f75ed614ca48c31d149954ee6943a9f7765a
ranger163 <ahmed.nassar163@gmail.com>
2020-05-25 20:49:44 +0200

    Wrapping up all in the MovieDetailsActivity. Closes #12

--- app/src/main/java/me/inassar/tmbd/view/moviedetails/MovieDetailsActivity.kt
+++ app/src/main/java/me/inassar/tmbd/view/moviedetails/MovieDetailsActivity.kt
@@ -1,13 +1,78 @@
 package me.inassar.tmbd.view.moviedetails
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_movie_details.*
+import me.inassar.tmbd.Constants.POSTER_BASE_URL
 import me.inassar.tmbd.R
+import me.inassar.tmbd.data.api.ApiCall
+import me.inassar.tmbd.data.api.AppApi
+import me.inassar.tmbd.data.model.MovieDetailsModel
+import me.inassar.tmbd.data.repository.NetworkState
+import java.text.NumberFormat
+import java.util.*
 
 class MovieDetailsActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MovieDetailsViewModel
+    private lateinit var movieRepository: MovieDetailsRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+
+        val movieId: Int = intent.getIntExtra("id", 1)
+
+        val apiService: AppApi = ApiCall.getClient()
+        movieRepository = MovieDetailsRepository(apiService)
+
+        viewModel = getViewModel(movieId)
+
+        viewModel.movieDetails.observe(this, Observer {
+            bindUI(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            progress_bar.visibility = if (it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            txt_error.visibility = if (it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+        })
+
+    }
+
+    fun bindUI(it: MovieDetailsModel) {
+        movie_title.text = it.title
+        movie_tagline.text = it.tagline
+        movie_release_date.text = it.releaseDate
+        movie_rating.text = it.rating.toString()
+        movie_runtime.text = it.runtime.toString() + " minutes"
+        movie_overview.text = it.overview
+
+        val formatCurrency = NumberFormat.getCurrencyInstance(Locale.US)
+        movie_budget.text = formatCurrency.format(it.budget)
+        movie_revenue.text = formatCurrency.format(it.revenue)
+
+        val moviePosterURL = POSTER_BASE_URL + it.posterPath
+        Glide.with(this)
+            .load(moviePosterURL)
+            .into(iv_movie_poster);
+
+
+    }
+
+
+    private fun getViewModel(movieId: Int): MovieDetailsViewModel {
+        return ViewModelProviders.of(this, object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MovieDetailsViewModel(movieRepository, movieId) as T
+            }
+        })[MovieDetailsViewModel::class.java]
     }
 }
